49b379374a7d8e8d27e025b465fa10718e43726e
Diogo Valentim <diogovalentte10@gmail.com>
2024-05-02 14:30:17 -0300

    Update tracker to allow track self describing events

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,39 +1,47 @@
 import {
-    newTracker,
-    trackPageView,
-    enableActivityTracking,
+  newTracker,
+  trackPageView,
+  enableActivityTracking,
+  trackSelfDescribingEvent,
 } from "@snowplow/browser-tracker";
 
 import {
-    enableButtonClickTracking,
-    ButtonClickTrackingPlugin,
+  enableButtonClickTracking,
+  ButtonClickTrackingPlugin,
 } from "@snowplow/browser-plugin-button-click-tracking";
 
 import {
-    LinkClickTrackingPlugin,
-    enableLinkClickTracking,
+  LinkClickTrackingPlugin,
+  enableLinkClickTracking,
 } from "@snowplow/browser-plugin-link-click-tracking";
 
 export class SnowplowService {
-    public tracker: any;
+  public tracker: any;
 
-    constructor(appId: string) {
-        this.tracker = newTracker("dynMainTracker", "https://neo.dynaton.com.br", {
-            appId: appId,
-            platform: "web",
-            cookieSameSite: "Lax",
-            contexts: {
-                webPage: true,
-            },
-            plugins: [ButtonClickTrackingPlugin(), LinkClickTrackingPlugin()],
-        });
+  constructor(appId: string) {
+    this.tracker = newTracker("dynMainTracker", "https://neo.dynaton.com.br", {
+      appId: appId,
+      platform: "web",
+      cookieSameSite: "Lax",
+      contexts: {
+        webPage: true,
+      },
+      plugins: [ButtonClickTrackingPlugin(), LinkClickTrackingPlugin()],
+    });
 
-        enableActivityTracking({
-            minimumVisitLength: 5,
-            heartbeatDelay: 10,
-        });
-        enableButtonClickTracking();
-        enableLinkClickTracking({ pseudoClicks: true });
-        trackPageView();
-    }
+    enableActivityTracking({
+      minimumVisitLength: 5,
+      heartbeatDelay: 10,
+    });
+    enableButtonClickTracking();
+    enableLinkClickTracking({ pseudoClicks: true });
+  }
+
+  public async trackPageView() {
+    trackPageView();
+  }
+
+  public async trackSelfDescribingEvent(event: any) {
+    trackSelfDescribingEvent(event);
+  }
 }
